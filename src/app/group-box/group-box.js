aa37f7e0517d51f6a1ce42e7e87b816d593c8499
cangosta <luiz.santos@wintouch.pt>
2016-01-12 15:36:15 +0000

    Simplify even more

--- src/app/group-box/group-box.js
+++ src/app/group-box/group-box.js
@@ -1,12 +1,10 @@
 import { Component, View } from 'angular2/core';
-import { CORE_DIRECTIVES } from 'angular2/common';
 
 @Component({
   selector: 'group-box',
 })
 @View({
-  templateUrl: './group-box.html',
-  directives: [CORE_DIRECTIVES]
+  templateUrl: './group-box.html'
 })
 export class GroupBox {
 }
--- src/test/group-box.test.js
+++ src/test/group-box.test.js
@@ -10,7 +10,6 @@ import {
 } from 'angular2/testing';
 
 import { Component } from 'angular2/core';
-
 import { GroupBox } from '../app/group-box/group-box.js';
 
 @Component({
@@ -18,32 +17,23 @@ import { GroupBox } from '../app/group-box/group-box.js';
   directives: [GroupBox]
 })
 class TestComponent {
-  expandedCallback() {
-    this.expandedCalled = true;
-  }
 }
 
 describe('group-box control', () => {
 
   //'should raise expanded event on title click when it is expandable and it is collapsed'
-  it('testing...', inject([TestComponentBuilder], fakeAsync((tcb) => {
+  it('should fail', inject([TestComponentBuilder], fakeAsync((tcb) => {
     var fixture;
 
-    //tcb.overrideTemplate(TestComponent, '<group-box [isExpandable]="true" [isExpanded]="false" (expanded)="expandedCallback($event)"><group-box-header>Title Content</group-box-header>Body Content</group-box>')
-    var res = tcb.createAsync(TestComponent).then((rootFixture) => {
+    // tcb.overrideTemplate(TestComponent, '<group-box id="identifier"></group-box>')
+    tcb.createAsync(TestComponent).then((rootFixture) => {
         fixture = rootFixture;
       });
 
     tick();
 
     fixture.detectChanges();
-    var element = fixture.debugElement.componentInstance;
-    var compiled = fixture.debugElement.nativeElement;
 
-    compiled.querySelector('.group-box-header').click();
-
-    tick();
-    fixture.detectChanges();
     expect(false).toBeTruthy();
 
   })));
